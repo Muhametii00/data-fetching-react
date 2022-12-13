efc5cbe30be71e508dcb5d25a3e4d9ab3f02d9b1
Muhamet <konushevcimuhamet@gmail.com>
2022-12-13 21:14:27 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/shared/Button"
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Button name="Previous" />
+      <Button name="Next" />
+    </div>
+  )
+}
+
+export default App
